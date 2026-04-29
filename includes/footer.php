2d2336cc79ff233c066fc1639395d8a0d737e940
Byd1ego <javicastello2003@gmail.com>
2026-04-29 13:31:01 +0200

    He añadido contacto.php y he metido las imagenes en el admin

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 <footer>
-<p>tiendadepadel@gmail.com <br>666 666 666 | 999 999 999</p>
+<p>padelzone@gmail.com <br>666 666 666 | 999 999 999</p>
             <img src="static/img/logo.png" alt="" width="180">
             <p>Crevillente, España</p>
         </footer>
