8bbebe784164299d9036412162565903f935a522
Byd1ego <javicastello2003@gmail.com>
2026-05-18 13:17:54 +0200

    arreglo el archivo de configuracion para subirlo al hosting

--- includes/configuracion.php
+++ includes/configuracion.php
@@ -1,10 +1,17 @@
 <?php
 
-    //  Parametros de la conexión a la base de datos
-    define('DB_HOST', 'localhost');
-    define('DB_NAME', 'tienda_padel');
-    define('DB_USER', 'root');
-    define('DB_PASS', '');
-    define('DB_CHARSET', 'utf8mb4');
+    if ($_SERVER['HTTP_HOST'] === 'localhost') {
+    // Configuración local
+    $host = 'localhost';
+    $usuario = 'root';
+    $password = '';
+    $bd = 'tienda_padel';
+} else {
+    // Configuración InfinityFree
+    $host = 'sql208.infinityfree.com'; // el host que te dio InfinityFree
+    $usuario = 'if0_41953172';
+    $password = 'tu_password_bd';
+    $bd = 'tu_nombre_bd';
+}
 
 ?>
